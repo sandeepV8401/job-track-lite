feccc984b8509d11577d1f6cf5119b4df7a6af84
sandeep <73609003+sandeepV8401@users.noreply.github.com>
2026-01-13 14:45:15 +0530

    fix: vite.config.js for vercel deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
- 
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
